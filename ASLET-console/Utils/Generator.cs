f5227d2edcae84be76979a2d67a0545c7dea4453
CreatorX8 <RealCreatorX@gmail.com>
2022-10-30 16:17:27 +0200

    refactor method

--- ASLET-console/Utils/Generator.cs
+++ ASLET-console/Utils/Generator.cs
@@ -90,7 +90,7 @@ namespace ASLET.Utils
                     failedAttempts++;
                     continue;
                 }
-                lessonsCount = GetLessonCountForAWeek(schoolClass, currentLesson);
+                lessonsCount = GetLessonCountWeek(schoolClass, currentLesson);
                 if (lessonsCount >= currentLesson.maxAWeek)
                 {
                     failedAttempts++;
@@ -135,7 +135,7 @@ namespace ASLET.Utils
             return count;
         }
 
-        private byte GetLessonCountForAWeek(Class schoolClass, Lesson lesson)
+        private byte GetLessonCountWeek(Class schoolClass, Lesson lesson)
         {
             byte count = 0;
             if (Timetable.timetable.ContainsKey(schoolClass))
