964b3854cd057c06c1222a11b4df9ae9a2359b66
ivokraev <ivo.kraev2@protonmail.com>
2022-11-03 21:24:51 +0200

    Clean up and update Program.cs

--- ASLET-console/Program.cs
+++ ASLET-console/Program.cs
@@ -20,68 +20,27 @@ namespace ASLET
 
             //$ FIRST DEBUG OUTPUT
             //Console.WriteLine("Hello? SiR");
-            /* foreach (Tuple<Lesson, Teacher> currentLesson in generator.schedule)
-            {
-                Console.Write(currentLesson.Item1.displayName.PadRight(60));
-                Console.Write(currentLesson.Item2.name.PadRight(30));
-                Console.Write(String.Join(", ", currentLesson.Item2.freeLessons).PadRight(60));
-                int freeLessonsCount = 0;
-                foreach (bool lesson in currentLesson.Item2.freeLessons)
-                    if (lesson) freeLessonsCount++;
-                Console.WriteLine(freeLessonsCount);
-            } */
             Console.WriteLine("FINISHED!");
             Debug.SaveTimetable(Classes);
-            // foreach (Class schoolClass in Classes)
-            // {
-            //     Console.WriteLine(schoolClass.className);
-            //     foreach (List<Tuple<Lesson, Teacher>> currentDay in Timetable.timetable[schoolClass].Values)
-            //     {
-            //         Console.WriteLine();
-            //         // Console.WriteLine(
-            //         //     "--------------------------------------------------------------------------------------------------------------------------------------------");
-            //         foreach (Tuple<Lesson, Teacher> currentLesson in currentDay)
-            //         {
-            //             Console.Write(currentLesson.Item1.displayName.PadRight(60));
-            //             Console.WriteLine();
-            //             // Console.Write(currentLesson.Item2.name.PadRight(30));
-            //             // Console.Write(String.Join(", ", currentLesson.Item2.freeLessons).PadRight(60));
-            //             // int freeLessonsCount = 0;
-            //             // foreach (bool lesson in currentLesson.Item2.freeLessons)
-            //             //     if (lesson)
-            //             //         freeLessonsCount++;
-            //             // Console.WriteLine(freeLessonsCount);
-            //         }
-            //
-            //         // Console.WriteLine(
-            //         //     "--------------------------------------------------------------------------------------------------------------------------------------------");
-            //     }
-            // }
         }
 
         private static void FillLessons()
         {
-            Lessons.Add(new Lesson("Български език и литература", LessonType.Language, "bulgarian", Complexity.Normal,
-                2, 4));
+            Lessons.Add(new Lesson("Български език и литература", LessonType.Language, "bulgarian", Complexity.Normal, 2, 4));
             Lessons.Add(new Lesson("Математика", LessonType.Algorithmic, "math", Complexity.Hard, 2, 4));
             Lessons.Add(new Lesson("Философия", LessonType.Narrative, "philosophy", Complexity.Normal, 1, 2));
-            Lessons.Add(new Lesson("География и икономика", LessonType.Narrative, "geography", Complexity.Normal, 1,
-                2));
+            Lessons.Add(new Lesson("География и икономика", LessonType.Narrative, "geography", Complexity.Normal, 1, 2));
             Lessons.Add(new Lesson("Физика и астрономия", LessonType.Science, "physics", Complexity.Hard, 1, 2));
-            Lessons.Add(new Lesson("Биология и здравно образование", LessonType.Science, "biology", Complexity.Hard, 1,
-                2));
-            Lessons.Add(new Lesson("Химия и опазване на околната среда", LessonType.Science, "chemistry",
-                Complexity.Hard, 1, 2));
+            Lessons.Add(new Lesson("Биология и здравно образование", LessonType.Science, "biology", Complexity.Hard, 1, 2));
+            Lessons.Add(new Lesson("Химия и опазване на околната среда", LessonType.Science, "chemistry", Complexity.Hard, 1, 2));
             Lessons.Add(new Lesson("Музика", LessonType.Relaxing, "music", Complexity.Easy, 0, 0));
             Lessons.Add(new Lesson("Изобразително изкуство", LessonType.Relaxing, "art", Complexity.Easy, 1, 1));
             Lessons.Add(new Lesson("Информационни технологии", LessonType.Algorithmic, "it", Complexity.Easy, 0, 0));
-            Lessons.Add(new Lesson("Физическо възпитание и спорт", LessonType.Sport, "pe", Complexity.Easy, 1,
-                2));
+            Lessons.Add(new Lesson("Физическо възпитание и спорт", LessonType.Sport, "pe", Complexity.Easy, 1, 2));
             Lessons.Add(new Lesson("Английски език", LessonType.Language, "english", Complexity.Normal, 2, 2));
             Lessons.Add(new Lesson("Немски език", LessonType.Language, "german", Complexity.Normal, 2, 2));
             Lessons.Add(new Lesson("Час на класа", LessonType.Relaxing, "classhour", Complexity.Easy, 1, 1));
             Lessons.Add(new Lesson("История и цивилизации", LessonType.Narrative, "history", Complexity.Normal, 1, 4));
-            Lessons.Add(new Lesson("ПРАЗНО", LessonType.Nothing, "nothing", Complexity.Nothing, 0, 0));
         }
 
         private static void FillTeachers()
@@ -102,9 +61,8 @@ namespace ASLET
             Teachers.Add(new Teacher("Елена Дончева", "pe", BOTH));
             Teachers.Add(new Teacher("Ивелина Ю. Михайлова", "english", BOTH));
             Teachers.Add(new Teacher("Веселина Янкова", "german", BOTH));
-            // Teachers.Add(new Teacher("Павлина Й. Няголова", "classhour", BOTH));
             Teachers.Add(new Teacher("Кирил К. Димитров", "history", BOTH));
-            
+
             // Teachers.Add(new Teacher("Лилия Й. Колева2", "bulgarian", BOTH));
             // Teachers.Add(new Teacher("Павлина Я. Коларова2", "math", BOTH));
             // Teachers.Add(new Teacher("Ивелина Ю. Михайлова2", "philosophy", BOTH));
@@ -120,14 +78,12 @@ namespace ASLET
             // Teachers.Add(new Teacher("Веселина Янкова2", "german", BOTH));
             // Teachers.Add(new Teacher("Павлина Й. Няголова2", "classhour", BOTH));
             // Teachers.Add(new Teacher("Кирил К. Димитров2", "history", BOTH));
-            
-            Teachers.Add(new Teacher("НЯМА УТИТЕЛ", "nothing", new List<Class>()));
         }
 
         private static void FillClasses()
         {
-            Classes.Add(new Class("8А"));
-            Classes.Add(new Class("8Б"));
+            Classes.Add(new Class(8, 'А'));
+            Classes.Add(new Class(8, 'Б'));
         }
     }
 }
