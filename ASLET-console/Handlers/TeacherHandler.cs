86d51ac0af751edf5335141e38a86e9562dbd024
CreatorX8 <RealCreatorX@gmail.com>
2022-11-05 18:15:41 +0200

    create TeacherHandler.cs handler

---
+++ ASLET-console/Handlers/TeacherHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace ASLET.Handlers;
+
+public class TeacherHandler : IHandler
+{
+    private List<string> Teachers { get; set; }
+
+    public TeacherHandler()
+    {
+        Teachers = new List<string>();
+    }
+
+    public void Add(string name)
+    {
+        Teachers.Add(name);
+    }
+
+    public void Remove(string name)
+    {
+        Teachers.Remove(name);
+    }
+
+    public bool Contains(string name)
+    {
+        return Teachers.Contains(name);
+    }
+}
