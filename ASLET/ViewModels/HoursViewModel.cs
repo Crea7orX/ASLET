a2e95ee190159c9d742c892b77693f3998e92cb3
CreatorX8 <RealCreatorX@gmail.com>
2022-11-06 20:54:45 +0200

    create HoursView.axaml and HoursViewModel.cs

---
+++ ASLET/ViewModels/HoursViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace ASLET.ViewModels;
+
+public class HoursViewModel : ReactiveObject, IRoutableViewModel
+{
+    private static HoursViewModel? _instance;
+
+    public static HoursViewModel? GetInstance(IScreen hostScreen)
+    {
+        if (_instance == null)
+        {
+            _instance = new HoursViewModel(hostScreen);
+        }
+
+        return _instance;
+    }
+
+    public string? UrlPathSegment => "Hours";
+    public IScreen HostScreen { get; }
+
+    public HoursViewModel(IScreen hostScreen)
+    {
+        HostScreen = hostScreen;
+    }
+}
