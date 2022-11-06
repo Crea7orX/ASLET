1115e2d8157b27b3bab3f96566eb8f2008c29b27
CreatorX8 <RealCreatorX@gmail.com>
2022-11-06 20:54:21 +0200

    create SubjectsView.axaml and SubjectsViewModel.cs

---
+++ ASLET/ViewModels/SubjectsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace ASLET.ViewModels;
+
+public class SubjectsViewModel : ReactiveObject, IRoutableViewModel
+{
+    private static SubjectsViewModel? _instance;
+
+    public static SubjectsViewModel? GetInstance(IScreen hostScreen)
+    {
+        if (_instance == null)
+        {
+            _instance = new SubjectsViewModel(hostScreen);
+        }
+
+        return _instance;
+    }
+
+    public string? UrlPathSegment => "Subjects";
+    public IScreen HostScreen { get; }
+
+    public SubjectsViewModel(IScreen hostScreen)
+    {
+        HostScreen = hostScreen;
+    }
+}
---
+++ ASLET/Views/SubjectsView.axaml.cs
@@ -0,0 +1,35 @@
+﻿using ASLET.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace ASLET.Views;
+
+public partial class SubjectsView : ReactiveUserControl<SubjectsViewModel>
+{
+    private static SubjectsView? _instance;
+
+    public static SubjectsView? GetInstance(SubjectsViewModel viewModel)
+    {
+        if (_instance == null)
+        {
+            _instance = new SubjectsView
+            {
+                DataContext = viewModel
+            };
+        }
+
+        return _instance;
+    }
+
+    public SubjectsView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
