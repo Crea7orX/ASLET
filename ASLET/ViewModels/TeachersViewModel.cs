3a0b09066755583f59fa216d75f241fd701f4cab
CreatorX8 <RealCreatorX@gmail.com>
2022-11-06 10:56:39 +0200

    create TeachersView.axaml and TeachersViewModel.cs

---
+++ ASLET/ViewModels/TeachersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace ASLET.ViewModels;
+
+public class TeachersViewModel : ReactiveObject, IRoutableViewModel
+{
+    private static TeachersViewModel? _instance;
+
+    public static TeachersViewModel? GetInstance(IScreen hostScreen)
+    {
+        if (_instance == null)
+        {
+            _instance = new TeachersViewModel(hostScreen);
+        }
+
+        return _instance;
+    }
+
+    public string? UrlPathSegment => "Teachers";
+    public IScreen HostScreen { get; }
+
+    public TeachersViewModel(IScreen hostScreen)
+    {
+        HostScreen = hostScreen;
+    }
+}
---
+++ ASLET/Views/TeachersView.axaml.cs
@@ -0,0 +1,35 @@
+﻿using ASLET.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace ASLET.Views;
+
+public partial class TeachersView : ReactiveUserControl<TeachersViewModel>
+{
+    private static TeachersView? _instance;
+
+    public static TeachersView? GetInstance(TeachersViewModel viewModel)
+    {
+        if (_instance == null)
+        {
+            _instance = new TeachersView
+            {
+                DataContext = viewModel
+            };
+        }
+
+        return _instance;
+    }
+
+    public TeachersView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
