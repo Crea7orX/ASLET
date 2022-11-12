6b81334f40232d2036a32e01942044075d1a6998
CreatorX8 <RealCreatorX@gmail.com>
2022-11-12 09:39:46 +0200

    create HourModel.cs

---
+++ ASLET/Models/HourModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ASLET.Models;
+
+public class HourModel
+{
+    public byte Grade { get; }
+    public char Letter { get; }
+    public string? TeacherName { get; }
+    public string? SubjectName { get; }
+    public byte HoursAWeek { get; }
+
+    public HourModel(byte grade, char letter, string? teacherName, string? subjectName, byte hoursAWeek)
+    {
+        Grade = grade;
+        Letter = letter;
+        TeacherName = teacherName;
+        SubjectName = subjectName;
+        HoursAWeek = hoursAWeek;
+    }
+}
