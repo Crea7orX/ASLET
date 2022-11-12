dd4c7249826859257bd9c21fbbf6c7b3770f16e1
CreatorX8 <RealCreatorX@gmail.com>
2022-11-12 17:53:51 +0200

    add datagrid in ClassesView.axaml

--- ASLET/ViewModels/ClassesViewModel.cs
+++ ASLET/ViewModels/ClassesViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using ASLET.Models;
 using ReactiveUI;
@@ -27,10 +31,28 @@ public class ClassesViewModel : ViewModelBase, IRoutableViewModel
 
     public Interaction<ClassesDialogViewModel, ClassModel?> AddClass { get; }
 
+    private ClassModel _selectedClass;
+
+    public ClassModel SelectedClass
+    {
+        get => _selectedClass;
+        private set => this.RaiseAndSetIfChanged(ref _selectedClass, value);
+    }
+
+    private ObservableCollection<ClassModel> _classes = new();
+
+    public ObservableCollection<ClassModel> Classes
+    {
+        get => _classes;
+        private set => this.RaiseAndSetIfChanged(ref _classes, value);
+    }
+
+    public ICommand DeleteClassCommand { get; }
+
     public ClassesViewModel(IScreen hostScreen)
     {
         HostScreen = hostScreen;
-        
+
         AddClass = new Interaction<ClassesDialogViewModel, ClassModel?>();
         AddClassCommand = ReactiveCommand.CreateFromTask(async () =>
         {
@@ -38,6 +60,13 @@ public class ClassesViewModel : ViewModelBase, IRoutableViewModel
 
             // TODO ADD CLASS
             Console.WriteLine(result);
+            if (result != null) Classes.Add(result);
+        });
+
+        DeleteClassCommand = ReactiveCommand.CreateFromTask((ClassModel selectedClass) =>
+        {
+            Classes.Remove(selectedClass);
+            return Task.CompletedTask;
         });
     }
 }
