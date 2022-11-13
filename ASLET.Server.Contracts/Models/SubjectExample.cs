c7df7f92b0ce77b4f9822cee2e2238e16851166e
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 15:04:08 +0200

    create SubjectExample.cs model ASLET.Server.Contracts.csproj

---
+++ ASLET.Server.Contracts/Models/SubjectExample.cs
@@ -0,0 +1,13 @@
+﻿namespace ASLET.Server.Contracts.Models;
+
+public class SubjectExample
+{
+    public string Subject { get; }
+    public string Teacher { get; }
+
+    public SubjectExample(string subject, string teacher)
+    {
+        Subject = subject;
+        Teacher = teacher;
+    }
+}
