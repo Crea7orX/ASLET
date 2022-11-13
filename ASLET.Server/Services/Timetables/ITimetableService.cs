de15cf5249aa9a767c8eccb31731c198f29bc8c0
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 15:05:05 +0200

    create services ASLET.Server.csproj

---
+++ ASLET.Server/Services/Timetables/ITimetableService.cs
@@ -0,0 +1,12 @@
+﻿using ASLET.Server.Models;
+using ErrorOr;
+
+namespace ASLET.Server.Services.Timetables;
+
+public interface ITimetableService
+{
+    ErrorOr<Created> CreateTimetable(Timetable timetable);
+    ErrorOr<Timetable> GetTimetable(Guid id);
+    ErrorOr<UpsertedTimetable> UpsertTimetable(Timetable timetable);
+    ErrorOr<Deleted> DeleteTimetable(Guid id);
+}
---
+++ ASLET.Server/Services/Timetables/TimetableService.cs
@@ -0,0 +1,42 @@
+﻿using ASLET.Server.Models;
+using ASLET.Server.ServiceErrors;
+using ErrorOr;
+
+namespace ASLET.Server.Services.Timetables;
+
+public class TimetableService : ITimetableService
+{
+    private static readonly Dictionary<Guid, Timetable> Timetables = new();
+
+    public ErrorOr<Created> CreateTimetable(Timetable timetable)
+    {
+        Timetables.Add(timetable.Id, timetable);
+
+        return Result.Created;
+    }
+
+    public ErrorOr<Timetable> GetTimetable(Guid id)
+    {
+        if (Timetables.TryGetValue(id, out var timetable))
+        {
+            return timetable;
+        }
+
+        return Errors.Timetable.NotFound;
+    }
+
+    public ErrorOr<UpsertedTimetable> UpsertTimetable(Timetable timetable)
+    {
+        bool isNewlyCreated = !Timetables.ContainsKey(timetable.Id);
+        Timetables[timetable.Id] = timetable;
+
+        return new UpsertedTimetable(isNewlyCreated);
+    }
+
+    public ErrorOr<Deleted> DeleteTimetable(Guid id)
+    {
+        Timetables.Remove(id);
+
+        return Result.Deleted;
+    }
+}
