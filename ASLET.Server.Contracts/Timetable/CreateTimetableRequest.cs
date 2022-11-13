2462ae90cdd6586cfc858e315650ea663968e2ae
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 15:04:35 +0200

    create contracts ASLET.Server.Contracts.csproj

---
+++ ASLET.Server.Contracts/Timetable/CreateTimetableRequest.cs
@@ -0,0 +1,5 @@
+﻿using ASLET.Server.Contracts.Models;
+
+namespace ASLET.Server.Contracts.Timetable;
+
+public record CreateTimetableRequest(string ClassName, Dictionary<SubjectExample, int> Subjects, Guid CreatedBy);
