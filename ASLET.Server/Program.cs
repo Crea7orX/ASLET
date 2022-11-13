a3de8b783e6288cc096085a7cf0f2dcad1ae6c7d
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 12:46:28 +0200

    remove example files

--- ASLET.Server/Program.cs
+++ ASLET.Server/Program.cs
@@ -1,25 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+}
