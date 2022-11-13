d9ffb26bc200087b5c2b11be2092941ca9a5f319
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 15:05:52 +0200

    create controllers ASLET.Server.csproj

---
+++ ASLET.Server/Controllers/ErrorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASLET.Server.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        return Problem();
+    }
+}
