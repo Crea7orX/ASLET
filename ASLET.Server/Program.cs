a07911df67ffe2cef4262c708401c8d586c149db
CreatorX8 <RealCreatorX@gmail.com>
2022-11-13 15:03:10 +0200

    setup Program.cs

--- ASLET.Server/Program.cs
+++ ASLET.Server/Program.cs
@@ -1,10 +1,16 @@
+using ASLET.Server.Services.Timetables;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+
+    builder.Services.AddScoped<ITimetableService, TimetableService>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
+    
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
