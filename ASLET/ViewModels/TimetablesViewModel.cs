3cec0965b69bed34dda2b15605b1d88ac4520327
CreatorX8 <RealCreatorX@gmail.com>
2022-11-19 10:17:07 +0200

    create TimetablesView.axaml and TimetablesViewModel.cs

---
+++ ASLET/ViewModels/TimetablesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using ASLET.Services;
+using ReactiveUI;
+
+namespace ASLET.ViewModels;
+
+public class TimetablesViewModel : ViewModelBase, IRoutableViewModel
+{
+    private static TimetablesViewModel? _instance;
+
+    public static TimetablesViewModel GetInstance(IScreen? hostScreen)
+    {
+        if (_instance == null)
+        {
+            _instance = new TimetablesViewModel(hostScreen!);
+        }
+
+        return _instance;
+    }
+
+    public string? UrlPathSegment => "Timetables";
+    
+    public IScreen HostScreen { get; }
+
+    public ICommand GenerateTimetableCommand { get; }
+
+    public TimetablesViewModel(IScreen hostScreen)
+    {
+        HostScreen = hostScreen;
+        
+        GenerateTimetableCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            TimetableService.GenerateTimetable();
+        });
+    }
+}
