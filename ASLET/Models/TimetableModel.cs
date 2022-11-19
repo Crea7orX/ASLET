cecb0ba64dab33a5b39adf51a363dc3de0c3fcd2
CreatorX8 <RealCreatorX@gmail.com>
2022-11-19 15:38:57 +0200

    create TimetableModel.cs

---
+++ ASLET/Models/TimetableModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ASLET.Models;
+
+public class TimetableModel
+{
+    public string Hour { get; }
+    public string Monday { get; }
+    public string Tuesday { get; }
+    public string Wednesday { get; }
+    public string Thursday { get; }
+    public string Friday { get; }
+
+    public TimetableModel(string hour, string monday, string tuesday, string wednesday, string thursday, string friday)
+    {
+        Hour = hour;
+        Monday = monday;
+        Tuesday = tuesday;
+        Wednesday = wednesday;
+        Thursday = thursday;
+        Friday = friday;
+    }
+}
