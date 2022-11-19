ee7d94b444d7d8dcd81f6b1e8d333a508a329ff8
ivokraev <ivo.kraev2@protonmail.com>
2022-11-19 10:35:48 +0200

    Update routing

--- ASLET/ViewModels/MainWindowViewModel.cs
+++ ASLET/ViewModels/MainWindowViewModel.cs
@@ -1,32 +1,67 @@
-﻿using System.Reactive;
+﻿using System;
+using System.Reactive;
 using ReactiveUI;
 
 namespace ASLET.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase, IScreen
     {
+        private string _lastView = String.Empty;
+        
         public RoutingState Router { get; } = new RoutingState();
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToClasses { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToTeachers { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToSubjects { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToHours { get; }
+        private ReactiveCommand<Unit, IRoutableViewModel> _goToClasses { get; }
+        private ReactiveCommand<Unit, IRoutableViewModel> _goToTeachers { get; }
+        private ReactiveCommand<Unit, IRoutableViewModel> _goToSubjects { get; }
+        private ReactiveCommand<Unit, IRoutableViewModel> _goToHours { get; }
 
         public MainWindowViewModel()
         {
-            GoToClasses = ReactiveCommand.CreateFromObservable(
+            _goToClasses = ReactiveCommand.CreateFromObservable(
                 () => Router.Navigate.Execute(ClassesViewModel.GetInstance(this))
             );
-            GoToTeachers = ReactiveCommand.CreateFromObservable(
+            _goToTeachers = ReactiveCommand.CreateFromObservable(
                 () => Router.Navigate.Execute(TeachersViewModel.GetInstance(this))
             );
-            GoToSubjects = ReactiveCommand.CreateFromObservable(
+            _goToSubjects = ReactiveCommand.CreateFromObservable(
                 () => Router.Navigate.Execute(SubjectsViewModel.GetInstance(this))
             );
-            GoToHours = ReactiveCommand.CreateFromObservable(
+            _goToHours = ReactiveCommand.CreateFromObservable(
                 () => Router.Navigate.Execute(HoursViewModel.GetInstance(this))
             );
 
-            GoToClasses.Execute();
+            GoToClasses();
+        }
+        
+        public void GoToClasses()
+        {
+            if (_lastView == "ClassesViewModel") return;
+            
+            _lastView = "ClassesViewModel";
+            _goToClasses.Execute();
+        }
+        
+        public void GoToTeachers()
+        {
+            if (_lastView == "TeachersViewModel") return;
+            
+            _lastView = "TeachersViewModel";
+            _goToTeachers.Execute();
+        }
+        
+        public void GoToSubjects()
+        {
+            if (_lastView == "SubjectsViewModel") return;
+            
+            _lastView = "SubjectsViewModel";
+            _goToSubjects.Execute();
+        }
+        
+        public void GoToHours()
+        {
+            if (_lastView == "HoursDialogViewModel") return;
+            
+            _lastView = "HoursDialogViewModel";
+            _goToHours.Execute();
         }
     }
 }
