afea2b561a8dcab4d4a5d53c9cb05140b241ba38
CreatorX8 <RealCreatorX@gmail.com>
2022-11-19 11:51:01 +0200

    update Models

--- ASLET/Models/ClassModel.cs
+++ ASLET/Models/ClassModel.cs
@@ -1,13 +1,29 @@
-﻿namespace ASLET.Models;
+﻿using System;
+
+namespace ASLET.Models;
 
 public class ClassModel
 {
+    public Guid ClassId { get; }
     public byte Grade { get; }
     public char? Letter { get; }
 
     public ClassModel(byte grade = 0, char letter = ' ')
     {
+        ClassId = Guid.NewGuid();
         Grade = grade;
         Letter = letter;
+    }
+
+    public override string ToString()
+    {
+        return Grade.ToString() + Letter;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is HourModel) return ClassId == ((HourModel)obj).ClassId;
+        if (obj is ClassModel) return ((ClassModel)obj).ClassId == ClassId;
+        return false;
     }
 }
--- ASLET/Models/HourModel.cs
+++ ASLET/Models/HourModel.cs
@@ -1,19 +1,40 @@
-﻿namespace ASLET.Models;
+﻿using System;
+using ASLET.Services.Objects;
+
+namespace ASLET.Models;
 
 public class HourModel
 {
+    public Guid HourId { get; }
+    public Guid ClassId { get; }
     public byte Grade { get; }
     public char? Letter { get; }
+    public Guid TeacherId { get; }
     public string? TeacherName { get; }
+    public Guid SubjectId { get; }
     public string? SubjectName { get; }
     public byte HoursAWeek { get; }
 
-    public HourModel(byte grade, char? letter, string? teacherName, string? subjectName, byte hoursAWeek)
+    public HourModel(Guid classId, byte grade, char? letter, Guid teacherId, string? teacherName, Guid subjectId, string? subjectName, byte hoursAWeek)
     {
+        HourId = Guid.NewGuid();
+        ClassId = classId;
         Grade = grade;
         Letter = letter;
+        TeacherId = teacherId;
         TeacherName = teacherName;
+        SubjectId = subjectId;
         SubjectName = subjectName;
         HoursAWeek = hoursAWeek;
+    }
+
+    public string ClassToString()
+    {
+        return Grade.ToString() + Letter;
+    }
+
+    public SubjectExample GetSubject()
+    {
+        return new SubjectExample(SubjectName, TeacherName);
     }
 }
