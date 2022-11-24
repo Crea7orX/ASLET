d5b8e0086a783950081ea65617bda9167c73bad4
CreatorX8 <RealCreatorX@gmail.com>
2022-11-24 22:15:45 +0200

    add DbContext

--- ASLET.Server/Program.cs
+++ ASLET.Server/Program.cs
@@ -1,16 +1,22 @@
+using ASLET.Server.Context;
 using ASLET.Server.Services.Timetables;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
 
     builder.Services.AddScoped<ITimetableService, TimetableService>();
+
+    builder.Services.AddDbContext<Context>(options =>
+        options.UseMySql("server=db4free.net;uid=asletadmin;pwd=QKkRQ4b%KX5PkQiLQ1w7oBFvxib%r1",
+            ServerVersion.Parse("8.0.25-mysql")));
 }
 
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
-    
+
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
