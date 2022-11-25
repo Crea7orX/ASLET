3f71bf7daf506e73bc08c9a13814f629116c179d
CreatorX8 <RealCreatorX@gmail.com>
2022-11-25 20:49:58 +0200

    add TEMP logic

--- ASLET.Server/Program.cs
+++ ASLET.Server/Program.cs
@@ -1,6 +1,8 @@
+using Accord.Genetic;
+using ASLET.Server.Chromosomes;
 using ASLET.Server.Context;
+using ASLET.Server.Models;
 using ASLET.Server.Services.Timetables;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,9 +10,36 @@ var builder = WebApplication.CreateBuilder(args);
 
     builder.Services.AddScoped<ITimetableService, TimetableService>();
 
-    builder.Services.AddDbContext<Context>(options =>
-        options.UseMySql("server=db4free.net;uid=asletadmin;pwd=QKkRQ4b%KX5PkQiLQ1w7oBFvxib%r1",
-            ServerVersion.Parse("8.0.25-mysql")));
+    using (DatabaseContext databaseContext = new DatabaseContext())
+    {
+        Population population = new Population(1000, new TimetableChromosome(databaseContext),
+            new TimetableChromosome.FitnessFunction(), new EliteSelection());
+
+        int i = 0;
+        while (true)
+        {
+            population.RunEpoch();
+            i++;
+            if (population.FitnessMax >= 0.99 || i >= 1000)
+            {
+                break;
+            }
+        }
+
+        List<TimetableSlot> timetable = (population.BestChromosome as TimetableChromosome).Value.Select(chromosome =>
+            new TimetableSlot()
+            {
+                Lesson = chromosome.Lesson,
+                ClassId = "TOVA E TEST",
+                TeacherId = chromosome.TeacherId,
+                HourId = chromosome.HourId,
+                DayOfWeek = (DayOfWeek)chromosome.Day,
+                Id = Guid.NewGuid().ToString()
+            }
+        ).ToList();
+        databaseContext.TimetableSlots.AddRange(timetable);
+        databaseContext.SaveChanges();
+    }
 }
 
 var app = builder.Build();
