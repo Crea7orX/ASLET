eeca63db51686b6f1c134c4b9900c449671761b3
CreatorX8 <RealCreatorX@gmail.com>
2022-11-29 23:29:39 +0200

    create NotificationService.cs

---
+++ ASLET/Services/NotificationService.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Controls;
+using MessageBox.Avalonia.Enums;
+
+namespace ASLET.Services;
+
+public static class NotificationService
+{
+    public static void ShowSuccess(Window parent, string message)
+    {
+        MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Успешно", message, ButtonEnum.Ok, Icon.Success,
+            WindowStartupLocation.CenterOwner).ShowDialog(parent);
+    }
+    
+    public static void ShowWarning(Window parent, string message)
+    {
+        MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Внимание", message, ButtonEnum.Ok, Icon.Warning,
+            WindowStartupLocation.CenterOwner).ShowDialog(parent);
+    }
+
+    public static void ShowError(Window parent, string message)
+    {
+        MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Грешка", message, ButtonEnum.Ok, Icon.Error,
+            WindowStartupLocation.CenterOwner).ShowDialog(parent);
+    }
+}
